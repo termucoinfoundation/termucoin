dca3de3dae207e8419841361b1aab4c94b50f9fb
Quankey <aldiansosmed@gmail.com>
2026-08-16 11:34:22 +0000

    Fix: net.cpp typo

--- src/net.cpp
+++ src/net.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2016 The Bitcoin Core developers
-// Copyright (c) 2022 The Termucoin Core developers
+// Copyright (c) 2026 The Termucoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -1552,7 +1552,7 @@ void ThreadMapPort()
             }
         }
 
-        std::string strDesc = "Dogecoin " + FormatFullVersion();
+        std::string strDesc = "Termucoin " + FormatFullVersion();
 
         try {
             while (true) {
